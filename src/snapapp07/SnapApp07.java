508a59249d752348ee1be5d7b1d4030300e6a6f1
liancahyo12 <lian.cahyo@gmail.com>
2018-10-29 07:39:55 +0700

    initial preview

---
+++ src/snapapp07/SnapApp07.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp07;
+
+/**
+ *
+ * @author Lian CahJaya
+ */
+public class SnapApp07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SnapFrame tampil = new SnapFrame();
+        tampil.setVisible(true);        
+// TODO code application logic here
+    }
+    
+}
